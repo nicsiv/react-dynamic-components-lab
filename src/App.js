47013f9261a1a44468e51bfa21e0ae4dc5809b7e
nicsiv <nicoleusivek@gmail.com>
2021-02-22 00:38:56 -0600

    commit tree 5a8cedaebf3f872eb93d375979b69c9e717373e2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
+import Comment from './Comment.js'
 
 class App extends Component {
   
@@ -14,6 +15,7 @@ class App extends Component {
           <div id="seperator"></div>
           <div className="wrapper">
             <ColorBox opacity={1} />
+            <Comment />
           </div>
       </div>
     )
